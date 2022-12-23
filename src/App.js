a1884fc8a4d7ea98c3ef1d4604f980a548640664
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-12-23 21:49:43 +0900

    Install react router & make routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.scss";
-import WaldoImgContainer from "./components/WaldoImgContainer";
+import MapSelection from "./components/mapSelection/MapSelection";
+import WaldoImgContainer from "./components/waldoImgContainer/WaldoImgContainer";
 
 function App() {
   return (
     <div className='App'>
-      <WaldoImgContainer />
+      <Routes>
+        <Route path='/' element={<MapSelection />} />
+        <Route path='/map/:mapID' element={<WaldoImgContainer />} />
+      </Routes>
     </div>
   );
 }
