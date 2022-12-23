2537a78e9131e23e507d97e2cbd05303f051be08
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-12-23 21:50:49 +0900

    Add variable maps & coords with param

--- src/components/waldoImgContainer/WaldoImgContainer.js
+++ src/components/waldoImgContainer/WaldoImgContainer.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import waldoSnow from "../../assets/waldoSnow.jpg";
+import { useParams, Link } from "react-router-dom";
+
+import snowMap from "../../assets/waldoSnow.jpg";
+import cityMap from "../../assets/waldoCity.jpg";
+import deptMap from "../../assets/waldoDeptStore.jpg";
+import musketeersMap from "../../assets/waldoMusketeers.jpg";
+
 import odlaw from "../../assets/odlaw-face.jpg";
 import waldo from "../../assets/waldo-face.webp";
 import whitebeard from "../../assets/whitebeard-face.jpeg";
@@ -7,7 +13,11 @@ import loadingImg from "../../assets/loading.jpg";
 import "./waldoImgContainer.scss";
 
 const WaldoImg1 = () => {
-  const [charCoords, setCharCoords] = useState({
+  const { mapID } = useParams();
+
+  const [charCoords, setCharCoords] = useState({});
+
+  const snowCoords = {
     waldoMinX: 83.6,
     waldoMaxX: 87.5,
     waldoMinY: 44.8,
@@ -20,7 +30,52 @@ const WaldoImg1 = () => {
     odlawMaxX: 32.5,
     odlawMinY: 39.45,
     odlawMaxY: 41.5,
-  });
+  };
+
+  const cityCoords = {
+    waldoMinX: 41.8,
+    waldoMaxX: 44.8,
+    waldoMinY: 45,
+    waldoMaxY: 50,
+    whitebeardMinX: 65,
+    whitebeardMaxX: 67.5,
+    whitebeardMinY: 47.2,
+    whitebeardMaxY: 49,
+    odlawMinX: 57.6,
+    odlawMaxX: 59.6,
+    odlawMinY: 58.4,
+    odlawMaxY: 60.8,
+  };
+  const deptCoords = {
+    waldoMinX: 40.9,
+    waldoMaxX: 43.2,
+    waldoMinY: 10.35,
+    waldoMaxY: 13.5,
+    whitebeardMinX: 68.05,
+    whitebeardMaxX: 69.65,
+    whitebeardMinY: 2.05,
+    whitebeardMaxY: 4,
+    odlawMinX: 18.95,
+    odlawMaxX: 20.35,
+    odlawMinY: 44.75,
+    odlawMaxY: 48.05,
+  };
+  const muskCoords = {
+    waldoMinX: 52.17,
+    waldoMaxX: 53.15,
+    waldoMinY: 49.75,
+    waldoMaxY: 51.2,
+    whitebeardMinX: 93.25,
+    whitebeardMaxX: 94.79,
+    whitebeardMinY: 17.95,
+    whitebeardMaxY: 20.2,
+    odlawMinX: 14.7,
+    odlawMaxX: 15.5,
+    odlawMinY: 22.41,
+    odlawMaxY: 23.95,
+  };
+
+  const [mapSelection, setMapSelection] = useState();
   const [clickCoord, setClickCoord] = useState({});
   const [found, setFound] = useState({
     waldo: false,
@@ -40,6 +95,32 @@ const WaldoImg1 = () => {
   const [playerMessage, setPlayerMessage] = useState(
     "Click the screen to find the characters."
   );
+
+  useEffect(() => {
+    //TODO eventually use this to retrieve correct coords
+    //TODO find a better way of doing this - a reducer or soemthign.
+    //This will eventually retrieve info from online
+    switch (mapID) {
+      case "snowMap":
+        setMapSelection(snowMap);
+        setCharCoords(snowCoords);
+        break;
+      case "cityMap":
+        setMapSelection(cityMap);
+        setCharCoords(cityCoords);
+        break;
+      case "deptMap":
+        setMapSelection(deptMap);
+        setCharCoords(deptCoords);
+        break;
+      case "musketeersMap":
+        setMapSelection(musketeersMap);
+        setCharCoords(muskCoords);
+        break;
+      default:
+        console.log("Map name error.");
+    }
+  }, []);
 
   useEffect(() => {
     let interval = null;
@@ -92,10 +173,10 @@ const WaldoImg1 = () => {
     };
     setPopupStyle(popupStyle);
     setClickCoord({ x: xCoord, y: yCoord });
-    console.log(clickCoord);
   };
 
   const handleButtonClick = (char) => {
+    setPopupStyle({ display: "none" });
     const minX = `${char}MinX`;
     const maxX = `${char}MaxX`;
     const minY = `${char}MinY`;
@@ -150,7 +231,7 @@ const WaldoImg1 = () => {
 
           <div className='imgDiv'>
             <img
-              src={waldoSnow}
+              src={mapSelection}
               alt='Large crowd with waldo among them'
               onClick={handleClickCoord}
               id='waldoPic'
@@ -196,7 +277,9 @@ const WaldoImg1 = () => {
                   </div>
                 </form>
               </div>
-              <button className='homeButton'>Back to home</button>
+              <Link to='/'>
+                <button className='homeButton'>Back to home</button>
+              </Link>
             </div>
           </div>
           <div className='overlay'></div>
