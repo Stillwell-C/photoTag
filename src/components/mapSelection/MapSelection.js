e77d6bed9132cf393358a92f916a2867a90e175f
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-12-27 17:27:18 +0900

    Add usecontext and handle image loading
    Add usecontext to get coord/image data from context provider.
    Once data is recieved, useEffect queries server for image
    download URLs.
    Add loading image and text to placehold while images are retrieved
    from server.

--- src/components/mapSelection/MapSelection.js
+++ src/components/mapSelection/MapSelection.js
@@ -1,53 +1,136 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { ref, getDownloadURL } from "firebase/storage";
 
 import cityMap from "../../assets/waldoCity.jpg";
 import snowMap from "../../assets/waldoSnow.jpg";
 import deptMap from "../../assets/waldoDeptStore.jpg";
 import musketeersMap from "../../assets/waldoMusketeers.jpg";
+import loadingImg from "../../assets/waldoCharLoading.jpg";
 import "./mapSelection.scss";
+import { storage } from "../../firebase";
+import { WaldoInfoContext } from "../../DataContext";
 
 const MapSelection = () => {
+  const { waldoInfo, setWaldoInfo } = useContext(WaldoInfoContext);
+
+  const [mapImages, setMapImages] = useState(null);
+
+  useEffect(() => {
+    const getImages = async () => {
+      try {
+        const cityMap = await getDownloadURL(
+          ref(storage, waldoInfo.images.waldoCity)
+        );
+        const snowMap = await getDownloadURL(
+          ref(storage, waldoInfo.images.waldoSnow)
+        );
+        const deptMap = await getDownloadURL(
+          ref(storage, waldoInfo.images.waldoDeptStore)
+        );
+        const musketeersMap = await getDownloadURL(
+          ref(storage, waldoInfo.images.waldoMusketeers)
+        );
+        setMapImages({
+          cityMap: cityMap,
+          snowMap: snowMap,
+          deptMap: deptMap,
+          musketeersMap: musketeersMap,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (waldoInfo !== null) getImages();
+  }, [waldoInfo]);
+
   return (
     <div className='container'>
       <h2>Map Selection</h2>
       <div className='mapList'>
         <div className='mapLine'>
-          <Link to='/map/cityMap'>
-            <div className='singleMap'>
-              <div className='mapText'>
-                <p>City</p>
+          {mapImages ? (
+            <Link to='/map/cityMap'>
+              <div className='singleMap'>
+                <div className='mapText'>
+                  <p>City</p>
+                </div>
+                <img src={mapImages.cityMap} alt='City map' />
               </div>
-              <img src={cityMap} alt='City map' />
-            </div>
-          </Link>
-          <Link to='/map/snowMap'>
-            <div className='singleMap'>
-              <div className='mapText'>
-                <p>Ski Slope</p>
+            </Link>
+          ) : (
+            <div className='singleMap loading'>
+              <div className='singleMap'>
+                <div className='loadingText'>
+                  <p>Loading</p>
+                </div>
+                <img src={loadingImg} alt='loading placeholder' />
               </div>
-              <img src={snowMap} alt='Ski slope map' />
             </div>
-          </Link>
+          )}
+          {mapImages ? (
+            <Link to='/map/snowMap'>
+              <div className='singleMap'>
+                <div className='mapText'>
+                  <p>Ski Slope</p>
+                </div>
+                <img src={mapImages.snowMap} alt='Ski slope map' />
+              </div>
+            </Link>
+          ) : (
+            <div className='singleMap loading'>
+              <div className='singleMap'>
+                <div className='loadingText'>
+                  <p>Loading</p>
+                </div>
+                <img src={loadingImg} alt='loading placeholder' />
+              </div>
+            </div>
+          )}
         </div>
 
         <div className='mapLine'>
-          <Link to='/map/deptMap'>
-            <div className='singleMap'>
-              <div className='mapText'>
-                <p>Department Store</p>
+          {mapImages ? (
+            <Link to='/map/deptMap'>
+              <div className='singleMap'>
+                <div className='mapText'>
+                  <p>Department Store</p>
+                </div>
+                <img src={mapImages.deptMap} alt='Department store map' />
               </div>
-              <img src={deptMap} alt='Department store map' />
-            </div>
-          </Link>
-          <Link to='/map/musketeersMap'>
-            <div className='singleMap'>
-              <div className='mapText'>
-                <p>Swashbuckling Musketeers</p>
+            </Link>
+          ) : (
+            <div className='singleMap loading'>
+              <div className='singleMap'>
+                <div className='loadingText'>
+                  <p>Loading</p>
+                </div>
+                <img src={loadingImg} alt='loading placeholder' />
               </div>
-              <img src={musketeersMap} alt='Swashbuckling musketeers map' />
             </div>
-          </Link>
+          )}
+          {mapImages ? (
+            <Link to='/map/musketeersMap'>
+              <div className='singleMap'>
+                <div className='mapText'>
+                  <p>Swashbuckling Musketeers</p>
+                </div>
+                <img
+                  src={mapImages.musketeersMap}
+                  alt='Swashbuckling musketeers map'
+                />
+              </div>
+            </Link>
+          ) : (
+            <div className='singleMap loading'>
+              <div className='singleMap'>
+                <div className='loadingText'>
+                  <p>Loading</p>
+                </div>
+                <img src={loadingImg} alt='loading placeholder' />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
