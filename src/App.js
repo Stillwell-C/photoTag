1a68f8ad4b8da0f8f048fb7f4a48c94d3301454f
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-12-27 17:24:57 +0900

    Add context provider
    Add context provider and use usestate to query server for
    coord/img data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
 
 import "./App.scss";
 import MapSelection from "./components/mapSelection/MapSelection";
 import WaldoImgContainer from "./components/waldoImgContainer/WaldoImgContainer";
+import { WaldoInfoContext } from "./DataContext";
+import { dataDocRef } from "./firebase";
 
 function App() {
+  const [waldoInfo, setWaldoInfo] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const recievedData = await getDoc(dataDocRef);
+        setWaldoInfo(recievedData.data());
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className='App'>
-      <Routes>
-        <Route path='/' element={<MapSelection />} />
-        <Route path='/map/:mapID' element={<WaldoImgContainer />} />
-      </Routes>
+      <WaldoInfoContext.Provider value={{ waldoInfo, setWaldoInfo }}>
+        <Routes>
+          <Route path='/' element={<MapSelection />} />
+          <Route path='/map/:mapID' element={<WaldoImgContainer />} />
+        </Routes>
+      </WaldoInfoContext.Provider>
     </div>
   );
 }
