2a6a84ea76056ab6a948354f09affe473119e834
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-12-27 18:49:57 +0900

    Connect to server to get coords/images

--- src/components/waldoImgContainer/WaldoImgContainer.js
+++ src/components/waldoImgContainer/WaldoImgContainer.js
@@ -1,81 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
+import { ref, getDownloadURL } from "firebase/storage";
+import { getDoc } from "firebase/firestore";
 
-import snowMap from "../../assets/waldoSnow.jpg";
-import cityMap from "../../assets/waldoCity.jpg";
-import deptMap from "../../assets/waldoDeptStore.jpg";
-import musketeersMap from "../../assets/waldoMusketeers.jpg";
-
-import odlaw from "../../assets/odlaw-face.jpg";
-import waldo from "../../assets/waldo-face.webp";
-import whitebeard from "../../assets/whitebeard-face.jpeg";
 import loadingImg from "../../assets/loading.jpg";
 import "./waldoImgContainer.scss";
+import { WaldoInfoContext } from "../../DataContext";
+import { storage, dataDocRef } from "../../firebase";
 
 const WaldoImg1 = () => {
   const { mapID } = useParams();
+  const { waldoInfo } = useContext(WaldoInfoContext);
 
   const [charCoords, setCharCoords] = useState({});
 
-  const snowCoords = {
-    waldoMinX: 83.6,
-    waldoMaxX: 87.5,
-    waldoMinY: 44.8,
-    waldoMaxY: 49.2,
-    whitebeardMinX: 6.0,
-    whitebeardMaxX: 9,
-    whitebeardMinY: 46.75,
-    whitebeardMaxY: 49.5,
-    odlawMinX: 31,
-    odlawMaxX: 32.5,
-    odlawMinY: 39.45,
-    odlawMaxY: 41.5,
-  };
-
-  const cityCoords = {
-    waldoMinX: 41.8,
-    waldoMaxX: 44.8,
-    waldoMinY: 45,
-    waldoMaxY: 50,
-    whitebeardMinX: 65,
-    whitebeardMaxX: 67.5,
-    whitebeardMinY: 47.2,
-    whitebeardMaxY: 49,
-    odlawMinX: 57.6,
-    odlawMaxX: 59.6,
-    odlawMinY: 58.4,
-    odlawMaxY: 60.8,
-  };
-  const deptCoords = {
-    waldoMinX: 40.9,
-    waldoMaxX: 43.2,
-    waldoMinY: 10.35,
-    waldoMaxY: 13.5,
-    whitebeardMinX: 68.05,
-    whitebeardMaxX: 69.65,
-    whitebeardMinY: 2.05,
-    whitebeardMaxY: 4,
-    odlawMinX: 18.95,
-    odlawMaxX: 20.35,
-    odlawMinY: 44.75,
-    odlawMaxY: 48.05,
-  };
-  const muskCoords = {
-    waldoMinX: 52.17,
-    waldoMaxX: 53.15,
-    waldoMinY: 49.75,
-    waldoMaxY: 51.2,
-    whitebeardMinX: 93.25,
-    whitebeardMaxX: 94.79,
-    whitebeardMinY: 17.95,
-    whitebeardMaxY: 20.2,
-    odlawMinX: 14.7,
-    odlawMaxX: 15.5,
-    odlawMinY: 22.41,
-    odlawMaxY: 23.95,
-  };
-
   const [mapSelection, setMapSelection] = useState();
+  const [charFaces, setCharFaces] = useState({});
   const [clickCoord, setClickCoord] = useState({});
   const [found, setFound] = useState({
     waldo: false,
@@ -87,7 +27,8 @@ const WaldoImg1 = () => {
     whitebeard: 1,
     odlaw: 1,
   });
-  const [loading, setLoading] = useState(false);
+  const [mapLoading, setMapLoading] = useState(true);
+  const [facesLoading, setFacesLoading] = useState(true);
   const [gameover, setGameover] = useState(false);
   const [seconds, setSeconds] = useState(0);
   const [timer, setTimer] = useState("");
@@ -97,30 +38,68 @@ const WaldoImg1 = () => {
   );
 
   useEffect(() => {
-    //TODO eventually use this to retrieve correct coords
-    //TODO find a better way of doing this - a reducer or soemthign.
-    //This will eventually retrieve info from online
-    switch (mapID) {
-      case "snowMap":
-        setMapSelection(snowMap);
-        setCharCoords(snowCoords);
-        break;
-      case "cityMap":
-        setMapSelection(cityMap);
-        setCharCoords(cityCoords);
-        break;
-      case "deptMap":
-        setMapSelection(deptMap);
-        setCharCoords(deptCoords);
-        break;
-      case "musketeersMap":
-        setMapSelection(musketeersMap);
-        setCharCoords(muskCoords);
-        break;
-      default:
-        console.log("Map name error.");
+    const loadBackgroundImg = async (mapName) => {
+      try {
+        const backgroundImg = await getDownloadURL(
+          ref(storage, waldoInfo.images[mapName])
+        );
+        setMapSelection(backgroundImg);
+        setFacesLoading(false);
+      } catch (err) {
+        console.log("Background Img: ", err.message);
+      }
+    };
+    const loadCharacterImg = async () => {
+      try {
+        const waldo = await getDownloadURL(
+          ref(storage, waldoInfo.images.waldoFace)
+        );
+        const odlaw = await getDownloadURL(
+          ref(storage, waldoInfo.images.odlawFace)
+        );
+        const whitebeard = await getDownloadURL(
+          ref(storage, waldoInfo.images.whitebeardFace)
+        );
+        const wenda = await getDownloadURL(
+          ref(storage, waldoInfo.images.wendaFace)
+        );
+
+        setCharFaces({
+          waldoFace: waldo,
+          odlawFace: odlaw,
+          whitebeardFace: whitebeard,
+          wendaFace: wenda,
+        });
+        setMapLoading(false);
+      } catch (err) {
+        console.log("Character Img: ", err.message);
+      }
+    };
+
+    if (waldoInfo !== null) {
+      loadCharacterImg();
+      switch (mapID) {
+        case "snowMap":
+          loadBackgroundImg("waldoSnow");
+          setCharCoords(waldoInfo.coords.snowCoords);
+          break;
+        case "cityMap":
+          loadBackgroundImg("waldoCity");
+          setCharCoords(waldoInfo.coords.cityCoords);
+          break;
+        case "deptMap":
+          loadBackgroundImg("waldoDeptStore");
+          setCharCoords(waldoInfo.coords.deptCoords);
+          break;
+        case "musketeersMap":
+          loadBackgroundImg("waldoMusketeers");
+          setCharCoords(waldoInfo.coords.muskCoords);
+          break;
+        default:
+          console.log("Loading Error.");
+      }
     }
-  }, []);
+  }, [waldoInfo]);
 
   useEffect(() => {
     let interval = null;
@@ -199,30 +178,30 @@ const WaldoImg1 = () => {
 
   return (
     <div className='container'>
-      {loading && (
+      {(facesLoading || mapLoading) && (
         <div className='loadingDiv'>
           <img src={loadingImg} alt='Waldo for loading screen' />
           <p className='loadingText'>Loading...</p>
         </div>
       )}
-      {!loading && (
+      {!facesLoading && !mapLoading && (
         <>
           <div className='gameInfo'>
             <div className='timerDiv'>{timer}</div>
             <div className='playerMessage'>{playerMessage}</div>
             <div className='characterDisplay'>
               <img
-                src={waldo}
+                src={charFaces.waldoFace}
                 alt='Character Waldo'
                 style={{ opacity: charOpac.waldo }}
               />
               <img
-                src={odlaw}
+                src={charFaces.odlawFace}
                 alt='Character Odlaw'
                 style={{ opacity: charOpac.odlaw }}
               />
               <img
-                src={whitebeard}
+                src={charFaces.whitebeardFace}
                 alt='Character Whitebeard'
                 style={{ opacity: charOpac.whitebeard }}
               />
