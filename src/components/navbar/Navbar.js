5ce9406f0e21a5d9ecb10a4570e1c51bbaac387a
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-12-28 16:20:56 +0900

    Add navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+      <div className='links'>
+        <Link to='/'>Home</Link>
+        <Link to='/leaderboards'>LeaderBoard</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
