aad53b53a0c73e561c18ee8372b5f5b63c0eb876
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-12-28 15:52:08 +0900

    Add leaderboard page

---
+++ src/components/leaderBoard/LeaderBoard.js
@@ -0,0 +1,137 @@
+import { collection, query, orderBy, limit, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+import { db } from "../../firebase";
+import LoadingPage from "../loadingPage/LoadingPage";
+import "./leaderboard.scss";
+
+const LeaderBoard = () => {
+  const snowLead = query(
+    collection(db, "snowLeaderboard"),
+    orderBy("seconds"),
+    limit(5)
+  );
+  const cityLead = query(
+    collection(db, "cityLeaderboard"),
+    orderBy("seconds"),
+    limit(5)
+  );
+  const deptLead = query(
+    collection(db, "deptLeaderboard"),
+    orderBy("seconds"),
+    limit(5)
+  );
+  const muskLead = query(
+    collection(db, "musketeersLeaderboard"),
+    orderBy("seconds"),
+    limit(5)
+  );
+
+  const [leaderData, setLeaderData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const queryBoards = async () => {
+      try {
+        const snowCol = await getDocs(snowLead);
+        const snowArr = [];
+        snowCol.forEach((doc) => snowArr.push({ id: doc.id, ...doc.data() }));
+        const cityCol = await getDocs(cityLead);
+        const cityArr = [];
+        cityCol.forEach((doc) => cityArr.push({ id: doc.id, ...doc.data() }));
+        const deptCol = await getDocs(deptLead);
+        const deptArr = [];
+        deptCol.forEach((doc) => deptArr.push({ id: doc.id, ...doc.data() }));
+        const muskCol = await getDocs(muskLead);
+        const muskArr = [];
+        muskCol.forEach((doc) => muskArr.push({ id: doc.id, ...doc.data() }));
+        setLeaderData({
+          snowMap: snowArr,
+          cityMap: cityArr,
+          deptMap: deptArr,
+          muskMap: muskArr,
+        });
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    queryBoards();
+  }, []);
+
+  return (
+    <>
+      {loading && <LoadingPage />}
+      {!loading && (
+        <div className='container'>
+          <h2 className='title'>LeaderBoard</h2>
+          <div className='leaderboard-container'>
+            <div className='leaderboard-line'>
+              <div className='single-leaderboard'>
+                <h3>Ski Slope Map</h3>
+                <div className='leaderboard-div'>
+                  {leaderData.snowMap.map((entry) => (
+                    <div className='entry-div' key={entry.id}>
+                      <div className='entryName'>{entry.name}</div>
+                      <div className='entryTime'>{entry.timer}</div>
+                    </div>
+                  ))}
+                  {leaderData.snowMap.length < 1 && (
+                    <div className='no-data'>No data yet</div>
+                  )}
+                </div>
+              </div>
+              <div className='single-leaderboard'>
+                <h3>City Map</h3>
+                <div className='leaderboard-div'>
+                  {leaderData.cityMap.map((entry) => (
+                    <div key={entry.id}>
+                      <div className='entryName'>{entry.name}</div>
+                      <div className='entryTime'>{entry.timer}</div>
+                    </div>
+                  ))}
+                  {leaderData.cityMap.length < 1 && (
+                    <div className='no-data'>No data yet</div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className='leaderboard-line'>
+              <div className='single-leaderboard'>
+                <h3>Deptartment Store Map</h3>
+                <div className='leaderboard-div'>
+                  {leaderData.deptMap.map((entry) => (
+                    <div key={entry.id}>
+                      <div className='entryName'>{entry.name}</div>
+                      <div className='entryTime'>{entry.timer}</div>
+                    </div>
+                  ))}
+                  {leaderData.deptMap.length < 1 && (
+                    <div className='no-data'>No data yet</div>
+                  )}
+                </div>
+              </div>
+              <div className='single-leaderboard'>
+                <h3>Swashbuckling Musketeers Map</h3>
+                <div className='leaderboard-div'>
+                  {leaderData.muskMap.map((entry) => (
+                    <div key={entry.id}>
+                      <div className='entryName'>{entry.name}</div>
+                      <div className='entryTime'>{entry.timer}</div>
+                    </div>
+                  ))}
+                  {leaderData.muskMap.length < 1 && (
+                    <div className='no-data'>No data yet</div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LeaderBoard;
