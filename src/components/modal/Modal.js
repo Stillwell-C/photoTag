60399c20288d09df4fcae87412396f83b04b9aa0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-01-02 13:05:02 +0900

    Add & style submitting loading screen

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./modal.scss";
 
 const Modal = ({
   timer,
@@ -6,34 +7,57 @@ const Modal = ({
   setInputVal,
   disableSubmit,
   submitErrorMsg,
+  submitting,
 }) => {
   return (
     <>
       <div className='modalContainer'>
-        <div className='modalHeader'>Congratulations!</div>
-        <div className='modalBody'>
-          <div className='modalInfo'>Your time was {timer}</div>
-          <div className='modalForm'>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor='name'>Submit your score:</label>
-              <div className='inputDiv'>
-                <input
-                  onChange={(e) => setInputVal(e.target.value)}
-                  type='text'
-                  id='name'
-                  placeholder='Name'
-                />
-                <button type='submit' disabled={disableSubmit}>
-                  Submit
-                </button>
+        {!submitting && (
+          <>
+            <div className='modalHeader'>Congratulations!</div>
+            <div className='modalBody'>
+              <div className='modalInfo'>Your time was {timer}</div>
+              <div className='modalForm'>
+                <form onSubmit={handleSubmit}>
+                  <label htmlFor='name'>Submit your score:</label>
+                  <div className='inputDiv'>
+                    <input
+                      onChange={(e) => setInputVal(e.target.value)}
+                      type='text'
+                      id='name'
+                      placeholder='Name'
+                    />
+                    <button type='submit' disabled={disableSubmit}>
+                      Submit
+                    </button>
+                  </div>
+                  <div className='errorMsg'>{submitErrorMsg}</div>
+                </form>
               </div>
-              <div className='errorMsg'>{submitErrorMsg}</div>
-            </form>
+              <Link to='/'>
+                <button className='homeButton'>Back to home</button>
+              </Link>
+            </div>
+          </>
+        )}
+        {submitting && (
+          <div className='loadingDiv'>
+            <div className='lds-default' data-testid='loading-animation'>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
           </div>
-          <Link to='/'>
-            <button className='homeButton'>Back to home</button>
-          </Link>
-        </div>
+        )}
       </div>
       <div className='overlay'></div>
     </>
