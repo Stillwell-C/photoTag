ecdf568484023e5b6f7bcd0e2a7942def5e7823f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-03-18 12:31:18 +0900

    Add leaderboard & error pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import React, { useEffect, useState, useMemo } from "react";
-import { doc, getDoc } from "firebase/firestore";
+import { getDoc } from "firebase/firestore";
 
 import "./App.scss";
 import MapSelection from "./components/mapSelection/MapSelection";
 import WaldoImgContainer from "./components/waldoImgContainer/WaldoImgContainer";
 import { WaldoInfoContext } from "./DataContext";
 import { dataDocRef } from "./firebase";
+import ErrorPage from "./components/errorPage/ErrorPage";
+import LeaderBoard from "./components/leaderBoard/LeaderBoard";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   const [waldoInfo, setWaldoInfo] = useState(null);
 
+  //Removed this temporarily because it was interferring with testing
+  //TODO: figure out how to implement with functioning tests
   const memoizedWaldoInfo = useMemo(() => ({ waldoInfo }), [waldoInfo]);
+  console.log("memo", memoizedWaldoInfo);
 
   useEffect(() => {
     const getData = async () => {
@@ -28,10 +34,13 @@ function App() {
 
   return (
     <div className='App'>
+      <Navbar />
       <WaldoInfoContext.Provider value={memoizedWaldoInfo}>
         <Routes>
           <Route path='/' element={<MapSelection />} />
           <Route path='/map/:mapID' element={<WaldoImgContainer />} />
+          <Route path='/leaderboards' element={<LeaderBoard />} />
+          <Route path='*' element={<ErrorPage />} />
         </Routes>
       </WaldoInfoContext.Provider>
     </div>
