32e7ada3b9f864efa17e1a5b2676863a77739b42
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-18 19:26:34 +0900

    Add axios

--- src/components/mapSelection/MapSelection.js
+++ src/components/mapSelection/MapSelection.js
@@ -1,86 +1,91 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import photoTagApi from "../../app/api/photoTagApi";
 
 import "./mapSelection.scss";
 import { getURL } from "../../firebase";
 import { WaldoInfoContext } from "../../DataContext";
-import axios from "axios";
 
 const MapSelection = () => {
   const { waldoInfo } = useContext(WaldoInfoContext);
 
-  const [mapImages, setMapImages] = useState(null);
-  const [loadingArr, setLoadingArr] = useState([1, 2, 3, 4]);
+  const navigate = useNavigate();
+
+  const [mapImages, setMapImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const imgURL = (imgKey) =>
+    `https://res.cloudinary.com/danscxcd2/image/upload/${imgKey}`;
 
   useEffect(() => {
     if (waldoInfo !== null) getMaps();
   }, [waldoInfo]);
 
-  useEffect(() => {
-    const axioscall = async () => {
-      try {
-        // const response = await photoTagApi.get("/map/frontpage");
-        // const response = await axios.get("http://localhost:3500/map");
-        // console.log(response);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    axioscall();
-  }, []);
-
   const getMaps = async () => {
-    const mapLoadList = waldoInfo.mapLoadList;
-    const mapArr = [];
-    for (let mapName of mapLoadList) {
-      try {
-        const URL = await getURL(waldoInfo.images[mapName].storageRef);
-        mapArr.push({ ...waldoInfo.images[mapName], mapURL: URL });
-      } catch (err) {
-        console.log(err.message);
-      }
+    setLoading(true);
+    try {
+      const { data } = await photoTagApi.get("/map/frontpage");
+
+      setMapImages(data);
+      setLoading(false);
+      console.log(data);
+    } catch (err) {
+      setLoading(false);
+      navigate("/error");
     }
-    // console.log(mapArr);
-    setMapImages(mapArr);
+
+    // const mapLoadList = waldoInfo.mapLoadList;
+    // const mapArr = [];
+    // for (let mapName of mapLoadList) {
+    //   try {
+    //     const URL = await getURL(waldoInfo.images[mapName].storageRef);
+    //     mapArr.push({ ...waldoInfo.images[mapName], mapURL: URL });
+    //   } catch (err) {
+    //     console.log(err.message);
+    //   }
+    // }
+    // // console.log(mapArr);
+    // setMapImages(mapArr);
   };
+
+  const loadingDiv = (key) => (
+    <div
+      className='singleMap loading'
+      key={key}
+      data-testid='loading-animation'
+    >
+      <div className='loadingText'>
+        <div className='lds-ring'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <div className='container'>
       <h2>Map Selection</h2>
       <div className='mapList'>
-        {mapImages
+        {!loading
           ? mapImages.map((singleMap) => (
-              <Link to={`/map/${singleMap.id}`} key={singleMap.id}>
+              <Link to={`/map/${singleMap?._id}`} key={singleMap?._id}>
                 <div className='singleMap'>
                   <div className='mapText'>
-                    <p>{singleMap.name}</p>
+                    <p>{singleMap?.mapName}</p>
                   </div>
                   <img
-                    src={singleMap.mapURL}
-                    alt={singleMap.altText}
+                    src={imgURL(singleMap.imgKey)}
+                    alt={singleMap.mapName}
                     loading='lazy'
                   />
                 </div>
               </Link>
             ))
-          : loadingArr.map((el) => (
-              <div
-                className='singleMap loading'
-                key={el}
-                data-testid='loading-animation'
-              >
-                <div className='loadingText'>
-                  <div className='lds-ring'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                </div>
-              </div>
-            ))}
+          : Array(4).map((el, i) => loadingDiv(i))}
       </div>
     </div>
   );
