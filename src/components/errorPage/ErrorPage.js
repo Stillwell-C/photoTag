64cf6824d7b39dd41d94ec30fd460825a0b57e87
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-19 11:42:52 +0900

    Add conditional error message

--- src/components/errorPage/ErrorPage.js
+++ src/components/errorPage/ErrorPage.js
@@ -1,11 +1,27 @@
 import "./errorPage.scss";
 import errorImg from "../../assets/wrongPage.jpg";
+import { useLocation } from "react-router-dom";
 
 const ErrorPage = () => {
+  const location = useLocation();
+  let errorMessage = "Something went wrong";
+  console.log(location?.state);
+
+  if (
+    location?.state?.errorCode === "ERR_NETWORK" ||
+    location?.state?.errorCode === 500
+  ) {
+    errorMessage = "Network Error. Please try again later.";
+  }
+  if (location?.state?.errorCode === 404) {
+    errorMessage = "This page does not exist";
+  }
+
   return (
     <div className='errorPage'>
-      <h2>Error. This page does not exist</h2>
-      <img src={errorImg} alt='Error. This page does not exist.' />
+      <h2>Error</h2>
+      <p>{errorMessage}</p>
+      <img src={errorImg} alt='' />
     </div>
   );
 };
