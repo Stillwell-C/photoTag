d9af11b6ea9f294cec1d6f010c9351ff421d76b1
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-25 17:16:55 +0900

    Initial commit

---
+++ src/components/leaderBoard/LeaderboardTestData.ts
@@ -0,0 +1,270 @@
+export const multiBoardData = {
+  data: [
+    {
+      mapName: "map one",
+      leaderData: [
+        {
+          id: 1,
+          playerName: "player 1",
+          seconds: 1,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 2,
+          playerName: "player 2",
+          seconds: 2,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 3,
+          playerName: "player 3",
+          seconds: 3,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 4,
+          playerName: "player 4",
+          seconds: 4,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 5,
+          playerName: "player 5",
+          seconds: 5,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+      ],
+    },
+    {
+      mapName: "map two",
+      leaderData: [
+        {
+          id: 1,
+          playerName: "player 1",
+          seconds: 1,
+          timer: "00:00:00",
+          mapID: 2,
+        },
+        {
+          id: 2,
+          playerName: "player 2",
+          seconds: 2,
+          timer: "00:00:00",
+          mapID: 2,
+        },
+        {
+          id: 3,
+          playerName: "player 3",
+          seconds: 3,
+          timer: "00:00:00",
+          mapID: 2,
+        },
+        {
+          id: 4,
+          playerName: "player 4",
+          seconds: 4,
+          timer: "00:00:00",
+          mapID: 2,
+        },
+        {
+          id: 5,
+          playerName: "player 5",
+          seconds: 5,
+          timer: "00:00:00",
+          mapID: 2,
+        },
+      ],
+    },
+    {
+      mapName: "map three",
+      leaderData: [
+        {
+          id: 1,
+          playerName: "player 1",
+          seconds: 1,
+          timer: "00:00:00",
+          mapID: 3,
+        },
+        {
+          id: 2,
+          playerName: "player 2",
+          seconds: 2,
+          timer: "00:00:00",
+          mapID: 3,
+        },
+        {
+          id: 3,
+          playerName: "player 3",
+          seconds: 3,
+          timer: "00:00:00",
+          mapID: 3,
+        },
+        {
+          id: 4,
+          playerName: "player 4",
+          seconds: 4,
+          timer: "00:00:00",
+          mapID: 3,
+        },
+        {
+          id: 5,
+          playerName: "player 5",
+          seconds: 5,
+          timer: "00:00:00",
+          mapID: 3,
+        },
+      ],
+    },
+    {
+      mapName: "map four",
+      leaderData: [
+        {
+          id: 1,
+          playerName: "player 1",
+          seconds: 1,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 2,
+          playerName: "player 2",
+          seconds: 2,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 3,
+          playerName: "player 3",
+          seconds: 3,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 4,
+          playerName: "player 4",
+          seconds: 4,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 5,
+          playerName: "player 5",
+          seconds: 5,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+      ],
+    },
+  ],
+};
+
+export const singleBoardData = {
+  data: [
+    {
+      mapName: "map one",
+      leaderData: [
+        {
+          id: 1,
+          playerName: "player 1",
+          seconds: 1,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 2,
+          playerName: "player 2",
+          seconds: 2,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 3,
+          playerName: "player 3",
+          seconds: 3,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 4,
+          playerName: "player 4",
+          seconds: 4,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+        {
+          id: 5,
+          playerName: "player 5",
+          seconds: 5,
+          timer: "00:00:00",
+          mapID: 1,
+        },
+      ],
+    },
+  ],
+};
+
+export const emptyBoardData = {
+  data: [
+    {
+      mapName: "map one",
+      leaderData: [],
+    },
+  ],
+};
+
+export const tooManyPlayersData = {
+  data: [
+    {
+      mapName: "map four",
+      leaderData: [
+        {
+          id: 1,
+          playerName: "player 1",
+          seconds: 1,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 2,
+          playerName: "player 2",
+          seconds: 2,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 3,
+          playerName: "player 3",
+          seconds: 3,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 4,
+          playerName: "player 4",
+          seconds: 4,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 5,
+          playerName: "player 5",
+          seconds: 5,
+          timer: "00:00:00",
+          mapID: 4,
+        },
+        {
+          id: 6,
+          playerName: "player 6",
+          seconds: 6,
+          timer: "00:00:00",
+          mapID: 6,
+        },
+      ],
+    },
+  ],
+};
