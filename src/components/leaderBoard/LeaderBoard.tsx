c213a7805bbc1cbb79b69ee63008e3161a11c239
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-25 17:16:43 +0900

    Change individual leader boards to tables

--- src/components/leaderBoard/LeaderBoard.tsx
+++ src/components/leaderBoard/LeaderBoard.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import LoadingPage from "../loadingPage/LoadingPage";
 import "./leaderboard.scss";
 import photoTagApi from "../../app/api/photoTagApi";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
 
 type SingleLeader = {
   _id: string;
@@ -21,45 +23,64 @@ const LeaderBoard = () => {
   const [leaderData, setLeaderData] = useState<SingleMapData[]>([]);
   const [loading, setLoading] = useState<Boolean>(true);
 
+  const navigate = useNavigate();
+
   const getMapData = async () => {
-    const { data } = await photoTagApi.get("/leaderboard");
-    setLeaderData(data);
-    setLoading(false);
-    console.log(data);
+    try {
+      const { data } = await photoTagApi.get("/leaderboard");
+      setLeaderData(data);
+      setLoading(false);
+    } catch (e) {
+      const err = e as AxiosError;
+      setLoading(false);
+      if (err?.response) {
+        navigate("/error", {
+          state: { errorCode: err?.response?.status },
+        });
+      } else {
+        navigate("/error", {
+          state: { errorCode: err?.code },
+        });
+      }
+    }
   };
 
   useEffect(() => {
     getMapData();
   }, []);
 
+  const leaderBoardMapCards = leaderData?.map((singleBoard) => (
+    <article className='single-leaderboard' key={singleBoard?.mapName}>
+      <table>
+        <caption>{singleBoard?.mapName}</caption>
+        <tbody>
+          <tr className='entry-header'>
+            <th>Player</th>
+            <th>Time</th>
+          </tr>
+          {singleBoard?.leaderData?.slice(0, 5).map((entry, index) => (
+            <tr className='entry-div' key={entry._id}>
+              <td className='entryName'>{entry.playerName}</td>
+              <td className='entryTime'>{entry.timer}</td>
+            </tr>
+          ))}
+          {!(singleBoard?.leaderData?.length > 0) && (
+            <tr className='entry-div'>
+              <td className='no-data'>No data yet</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </article>
+  ));
+
   return (
     <>
       {loading && <LoadingPage />}
-      {!loading && leaderData.length > 0 && (
+      {leaderData.length > 0 && (
         <div className='container'>
           <h2 className='title'>Leaderboards</h2>
-          <div className='leaderboard-container'>
-            {leaderData?.map((singleBoard) => (
-              <div className='single-leaderboard' key={singleBoard?.mapName}>
-                <h3>{singleBoard?.mapName}</h3>
-                <div className='entry-header'>
-                  <div>Player</div>
-                  <div>Time</div>
-                </div>
-                <div className='leaderboard-div'>
-                  {singleBoard?.leaderData?.map((entry, index) => (
-                    <div className='entry-div' key={entry._id}>
-                      <div className='entryName'>{entry.playerName}</div>
-                      <div className='entryTime'>{entry.timer}</div>
-                    </div>
-                  ))}
-                  {!(singleBoard?.leaderData?.length > 0) && (
-                    <div className='no-data'>No data yet</div>
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
+          <div className='leaderboard-container'>{leaderBoardMapCards}</div>
         </div>
       )}
     </>
