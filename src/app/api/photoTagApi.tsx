1fd45cead73b44683e835731cb29cb7e69bef78f
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-26 16:30:50 +0900

    Update baseURL

--- src/app/api/photoTagApi.tsx
+++ src/app/api/photoTagApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const photoTagApi = axios.create({
-  baseURL: "http://phototagapi-production.up.railway.app/",
+  baseURL: "https://phototagapi-production.up.railway.app/",
   withCredentials: true,
 });
 
