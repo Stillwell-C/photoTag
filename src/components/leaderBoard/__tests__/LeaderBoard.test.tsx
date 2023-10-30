f826e9827707f3cb0f9f142b9c16e8f36d070ae6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-10-30 17:10:49 +0900

    Fix act warning with findByRole

--- src/components/leaderBoard/__tests__/LeaderBoard.test.tsx
+++ src/components/leaderBoard/__tests__/LeaderBoard.test.tsx
@@ -34,7 +34,7 @@ describe("LeaderBoard component", () => {
   afterEach(() => jest.clearAllMocks());
   afterEach(() => jest.restoreAllMocks());
 
-  it("renders a loading screen before API response", () => {
+  it("renders a loading screen before API response", async () => {
     apiGetMock.mockResolvedValue(multiBoardData);
 
     setup();
@@ -42,66 +42,55 @@ describe("LeaderBoard component", () => {
     const text = screen.getByText(/Loading.../i);
     expect(img).toBeInTheDocument();
     expect(text).toBeInTheDocument();
+
+    //Avoid act error
+    await screen.findByRole("heading", {
+      name: "Leaderboards",
+    });
   });
 
   describe("renders leaderboards upon API response", () => {
     it("Renders a header after data has loaded", async () => {
       apiGetMock.mockResolvedValue(multiBoardData);
       setup();
-
       const header = await screen.findByRole("heading", {
         name: "Leaderboards",
       });
       expect(header).toBeInTheDocument();
     });
-
     it("Renders a card for each map recieved from the API", async () => {
       apiGetMock.mockResolvedValue(multiBoardData);
       setup();
-
       const mapCardHeading = await screen.findAllByText(/^map/i);
-
       expect(mapCardHeading).toHaveLength(4);
     });
-
     it("Only renders the number of maps recieved from the API", async () => {
       apiGetMock.mockResolvedValue(singleBoardData);
       setup();
-
       const mapCardHeading = await screen.findAllByText(/^map/i);
-
       expect(mapCardHeading).toHaveLength(1);
     });
-
     it("Renders 'No Data Yet' message if map has no player data", async () => {
       apiGetMock.mockResolvedValue(emptyBoardData);
       setup();
-
       const noDataMsg = await screen.findByText(/no data yet/i);
-
       expect(noDataMsg).toBeInTheDocument();
     });
-
     it("Renders a maximum of 5 players", async () => {
       apiGetMock.mockResolvedValue(tooManyPlayersData);
       setup();
-
       const displayedPlayers = await screen.findAllByText(/00:00:00/i);
-
       expect(displayedPlayers).toHaveLength(5);
     });
-
     it("Renders a player and time column in each list", async () => {
       apiGetMock.mockResolvedValue(singleBoardData);
       setup();
-
       const playerColumn = await screen.findAllByText(/^player/i);
       const timeColumn = await screen.findAllByText(/00:00:00/i);
       //An extra to account for the title
       expect(playerColumn).toHaveLength(6);
       expect(timeColumn).toHaveLength(5);
     });
-
     it("Handles error by navigating to a new page", async () => {
       apiGetMock.mockRejectedValue({
         code: "ERR_NETWORK",
