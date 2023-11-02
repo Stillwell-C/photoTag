2ad4fbd4906cff62bb2f391c15ec3787943488a0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-02 18:04:43 +0900

    Add tailwind classes

--- src/components/loadingPage/LoadingPage.tsx
+++ src/components/loadingPage/LoadingPage.tsx
@@ -3,10 +3,16 @@ import "./loadingPage.scss";
 
 const LoadingPage = () => {
   return (
-    <div className='loadingContainer'>
-      <div className='loadingDiv'>
-        <img src={loadingImg} alt='loading' />
-        <h2 className='loadingText'>Loading...</h2>
+    <div className='flex w-full items-center justify-center overflow-hidden'>
+      <div className='responsive-card relative p-20'>
+        <img
+          className='animate-spin-slow rounded-full max-w-full opacity-60 p-2'
+          src={loadingImg}
+          alt='loading'
+        />
+        <h2 className='text-3xl 2xs:text-5xl xs:text-6xl sm:text-7xl font-semibold text-white absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>
+          Loading...
+        </h2>
       </div>
     </div>
   );
