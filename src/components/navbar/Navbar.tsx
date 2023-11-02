cc37561eb93ef614f2b9c8628ddea81f6ed96232
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-02 14:16:33 +0900

    Add tailwind classes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
-import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <nav className='navbar-container'>
-      <div className='links'>
-        <Link to='/'>Home</Link>
-        <Link to='/leaderboards'>Leaderboards</Link>
+    <nav className=' h-10 min-h-full bg-stone-300/30 flex items-center justify-end w-full md:mb-8 md:h-12'>
+      <div>
+        <Link
+          className=' mr-3 text-dec no-underline text-xl  text-black hover:opacity-80'
+          to='/'
+        >
+          Home
+        </Link>
+        <Link
+          className=' mr-3 text-dec no-underline text-xl text-black hover:opacity-80'
+          to='/leaderboards'
+        >
+          Leaderboards
+        </Link>
       </div>
     </nav>
   );
