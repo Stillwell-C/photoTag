7f3a90c652b02dc947ed14741a9924186e75958a
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-03 09:54:28 +0900

    Add height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./navbar/Navbar";
+import Footer from "./footer/Footer";
 
 const Layout = () => {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
