739dae01bcffc89814f779a0b3a0ec0c9f58b2c6
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-11-03 09:08:07 +0900

    Change html for accessibility & add img & heading

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
+import waldoSide from "../../assets/waldo-side.png";
 
 const Navbar = () => {
   return (
-    <nav className=' h-10 min-h-full bg-stone-300/30 flex items-center justify-end w-full md:mb-8 md:h-12'>
-      <div>
+    <header className=' h-10 min-h-full bg-stone-300/30 flex items-center justify-between w-full md:mb-8 md:h-12'>
+      <div className='flex h-full items-center'>
+        <img
+          src={waldoSide}
+          className='h-10 md:h-12 -scale-x-100'
+          alt=''
+          aria-hidden='true'
+        />
+        <h1 className='text-3xl -ml-2'>Phototag</h1>
+      </div>
+      <nav>
         <Link
           className=' mr-3 text-dec no-underline text-xl  text-black dark:text-neutral-50 hover:opacity-80'
           to='/'
@@ -16,8 +26,8 @@ const Navbar = () => {
         >
           Leaderboards
         </Link>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
 
