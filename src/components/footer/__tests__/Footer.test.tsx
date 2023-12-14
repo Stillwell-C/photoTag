02693b24ef8c4dfda7adf6e83b3c95570105e55d
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-12-14 11:01:21 +0900

    Wrap in render in browser router

--- src/components/footer/__tests__/Footer.test.tsx
+++ src/components/footer/__tests__/Footer.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Footer from "../Footer";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Footer component", () => {
   it("displays a link to my github", () => {
-    render(<Footer />);
+    render(
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+    );
     const link = screen.getByRole("link", { name: /stillwell-c/i });
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute("href", "https://github.com/Stillwell-C/");
